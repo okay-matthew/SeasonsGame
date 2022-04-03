962b157ce0d80ce0a4f213556e825c5e8f2b3d87
nbranch23 <nbranch@macalester.edu>
2022-04-02 20:34:48 -0500

    can't jump while in the air now! changed falling animation!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,17 +9,20 @@ public class PlayerMovement : MonoBehaviour
     private Animator am;
     private SpriteRenderer sprite;
 
-    public List<string> orbsList;
-    private int orbCount;
-    public Text orbText;
-
     private float x_dir = 0;
-    private bool grounded;
-
     [SerializeField] private float move_speed = 4f;
     [SerializeField] private float jump_force = 400f;
 
+    private bool grounded = false; //when true, player can jump
+    public Transform groundedChecker; //indicates where feet are to check if they're on the ground
+    public float checkGroundRadius; //radius around groundedChecker to see if its overlapping ground
+    public LayerMask groundLayer; //links to ground layer
+
     private enum PlayerState { idle, running, jumping, falling}
+
+    public List<string> orbsList;
+    private int orbCount;
+    public Text orbText;
     
 
     // Start is called before the first frame update
@@ -37,16 +40,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update() {
 
-        grounded = false;
+        Move();
 
-        x_dir = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(x_dir * move_speed, rb.velocity.y);
+        Jump();
 
-        if (Input.GetButtonDown("Jump")) {
-            // Debug.Log("hello");
-            rb.AddForce(new Vector2(0f, jump_force));
-            // rb.velocity = new Vector2(rb.velocity.x, jump_force);
-        }
+        CheckGrounded();
 
         UpdatePlayerAnimation();
         
@@ -72,13 +70,31 @@ public class PlayerMovement : MonoBehaviour
             state = PlayerState.jumping;
             
         }
-        else if (rb.velocity.y <= -0.1f) {
-            state = PlayerState.falling; //TODO: Must have 'ground' check to have animations work
+        //else if (rb.velocity.y <= -0.1f) { //different animation choice?
+        else if (grounded == false){ //i feel like theres a better way to do this
+            state = PlayerState.falling;
         }
 
         // Debug.Log(state);
 
         am.SetInteger("state", (int)state);
+    }
+
+    private void Move(){
+        x_dir = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(x_dir * move_speed, rb.velocity.y);
+    }
+
+    private void Jump(){
+         if (Input.GetButtonDown("Jump") && grounded) {
+            rb.AddForce(new Vector2(0f, jump_force));
+         }
+    }
+
+    private void CheckGrounded(){
+        //finds position of groundedChecker and uses the radius to determine whether the circle is overlapping anything on the ground layer
+        //returns true if its touching or false if its not
+        grounded = Physics2D.OverlapCircle(groundedChecker.position, checkGroundRadius, groundLayer);
     }
 
     // handles collisions
